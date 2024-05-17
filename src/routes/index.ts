465cc39877be18c1c34a2d234ba2e40ffdc063a7
niels-van-den-broeck <vandenbroeckniels@gmail.com>
2024-05-17 11:14:00 +0200

    update deps

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import { FastifyPluginCallback, RouteHandlerMethod } from 'fastify';
 
 const register: FastifyPluginCallback = (server, options, done) => {
   const getStatus: RouteHandlerMethod = async (request, reply) => {
-    reply.status(200).send('API is live');
+    return reply.redirect('https://www.google.com');
   };
 
   const successSchema = {};
